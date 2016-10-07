cd0d836021142e8dc46ee1344e0c151472521bb2
Michael Angerman <stormasm@gmail.com>
2016-10-07 09:03:05 -0700

    more noms cleanup

--- go/chunks/leveldb_store_test.go
+++ go/chunks/leveldb_store_test.go
@@ -5,7 +5,6 @@
 package chunks
 
 import (
-	"bytes"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -40,19 +39,4 @@ func (suite *LevelDBStoreTestSuite) TearDownTest() {
 	suite.Store.Close()
 	suite.factory.Shutter()
 	os.Remove(suite.dir)
-}
-
-func (suite *LevelDBStoreTestSuite) TestReservedKeys() {
-	// Apparently, the following:
-	//  s := []byte("")
-	//  s = append(s, 1, 2, 3)
-	//  f := append(s, 10, 20, 30)
-	//  g := append(s, 4, 5, 6)
-	//
-	// Results in both f and g being [1, 2, 3, 4, 5, 6]
-	// This was happening to us here, so ldb.chunkPrefix was "/chunk/" and ldb.rootKey was "/chun" instead of "/root"
-	ldb := suite.factory.CreateStore("").(*LevelDBStore)
-	suite.True(bytes.HasSuffix(ldb.rootKey, []byte(rootKeyConst)))
-	suite.True(bytes.HasSuffix(ldb.versionKey, []byte(versionKeyConst)))
-	suite.True(bytes.HasSuffix(ldb.chunkPrefix, []byte(chunkPrefixConst)))
 }
--- go/chunks/memory_store_test.go
+++ go/chunks/memory_store_test.go
@@ -5,7 +5,6 @@
 package chunks
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/attic-labs/testify/suite"
@@ -25,12 +24,4 @@ func (suite *MemoryStoreTestSuite) SetupTest() {
 
 func (suite *MemoryStoreTestSuite) TearDownTest() {
 	suite.Store.Close()
-}
-
-func (suite *MemoryStoreTestSuite) TestBadSerialization() {
-	bad := []byte{0, 1} // Not enough bytes to read first length
-	ms := NewMemoryStore()
-	suite.Panics(func() {
-		Deserialize(bytes.NewReader(bad), ms, nil)
-	})
 }
